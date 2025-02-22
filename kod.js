9f9f172b2d80500d57aa78dff1bf312f4fa26697
talhadrz <talhadurmaz36@gmail.com>
2025-02-23 00:26:23 +0300

    kopya eklendi

--- kod.js
+++ kod.js
@@ -1,10 +1,10 @@
 let secretNumber = Math.floor(Math.random() * 100) + 1; // 1-100 arası sayı seç
 let attempts = 0;
-
+ let sayi = secretNumber;
 function checkGuess() {
     let guess = parseInt(document.getElementById("guessInput").value);
     let message = "";
-
+   
     if (isNaN(guess) || guess < 1 || guess > 100) {
         message = "Lütfen 1 ile 100 arasında bir sayı girin!";
     } else {
@@ -17,8 +17,13 @@ function checkGuess() {
             message = `Tebrikler! ${attempts} denemede bildiniz. Yeni oyun başladı!`;
             secretNumber = Math.floor(Math.random() * 100) + 1; // Yeni sayı belirle
             attempts = 0; // Deneme sayısını sıfırla
+            sayi = secretNumber;
         }
     }
 
     document.getElementById("resultMessage").innerText = message;
+}
+function koy()
+{
+    document.getElementById("guessInput").value = sayi;   
 }
